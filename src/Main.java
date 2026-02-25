a8e8b9717b28f6724ec44539e3bf7d72bdf3da20
Kushagra Kapoor <kushagakapoor@gmail.com>
2026-02-25 09:02:45 +0530

    UC3: Palindrome using string reverse loop

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App");
 
-        String word = "madam"; // hardcoded input
+        Scanner sc = new Scanner(System.in);
 
-        String reversed = new StringBuilder(word).reverse().toString();
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
 
+        // 🔁 reverse using for loop (UC3 requirement)
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // 🔍 compare using equals()
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
         }
+
+        sc.close();
     }
 }
