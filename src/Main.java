a81af1fa85273bd5f5e11503fb3d4825832a189d
Kushagra Kapoor <kushagakapoor@gmail.com>
2026-02-25 09:57:28 +0530

    UC9: Recursive palindrome checker

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class Main {
 
 
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
+    public static boolean isPalindrome(String word, int left, int right) {
 
-        while (current != null) {
-            Node nextTemp = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextTemp;
+
+        if (left >= right) {
+            return true;
         }
-        return prev;
+
+
+        if (word.charAt(left) != word.charAt(right)) {
+            return false;
+        }
+
+
+        return isPalindrome(word, left + 1, right - 1);
     }
 
     public static void main(String[] args) {
@@ -31,51 +24,13 @@ public class Main {
         System.out.println("Welcome to Palindrome Checker App");
 
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a word: ");
         String word = sc.nextLine();
 
+        boolean result = isPalindrome(word, 0, word.length() - 1);
 
-        Node head = null, tail = null;
-
-        for (char ch : word.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-
-        Node secondHalf = reverse(slow);
-
-
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-
-        boolean isPalindrome = true;
-
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
-        }
-
-
-        if (isPalindrome) {
+        if (result) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
